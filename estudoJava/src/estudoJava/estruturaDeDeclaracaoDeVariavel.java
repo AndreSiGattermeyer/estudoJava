b9f7ccbb94cc3bc610ede4c6124b34ccb38b416b
AndreSiGattermeyer <asigatte@gmail.com>
2022-02-05 18:52:53 -0300

    Estrutura de declaração de variável

---
+++ estudoJava/src/estudoJava/estruturaDeDeclaracaoDeVariavel.java
@@ -0,0 +1,26 @@
+package estudoJava;
+
+public class estruturaDeDeclaracaoDeVariavel {
+
+	public static void main(String[] args) {
+		
+		int  maiorIdade =  18;
+		
+		int idosoIdade =  60;
+		
+		int jovemIdade = 16;
+		
+		int crianca = 7;
+		
+		String cpf = "145.785.789-54"; /*cpf é pessoal e intransferível*/
+		
+		System.out.println("Maior idade é " + maiorIdade);
+		System.out.println("idoso idade é " + idosoIdade);
+		System.out.println("Jovem idade é " + jovemIdade);
+		System.out.println("Criança idade é " + crianca );
+		
+
+	}
+
+}
+

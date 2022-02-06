333bb8abe62f171355c801abae8a1b529757e06b
AndreSiGattermeyer <asigatte@gmail.com>
2022-02-05 21:37:57 -0300

    Usando char

---
+++ estudoJava/src/estudoJava/caractereChar.java
@@ -0,0 +1,26 @@
+package estudoJava;
+
+public class caractereChar {
+
+	public static void main(String[] args) {
+		
+			/*Char representa uma letra*/
+			char pessoaFisica = 'F';
+		
+			char pessoaJuridica = 'J';
+				
+			char pessoaMasculino = 'M';
+		
+			char pessoaFeminino = 'd';
+			
+			if(pessoaFeminino == 'F') {
+				System.out.println("Feminino");
+			}else {
+				System.out.println("Masculino");
+			}
+		
+		
+
+	}
+
+}

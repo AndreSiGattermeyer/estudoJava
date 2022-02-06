f07e52a6c6070261592cc8126b38c53441d9b436
AndreSiGattermeyer <asigatte@gmail.com>
2022-02-05 21:21:17 -0300

    média com números inteiros

---
+++ estudoJava/src/estudoJava/operandoNumerosInteiros.java
@@ -0,0 +1,18 @@
+package estudoJava;
+
+public class operandoNumerosInteiros {
+
+	public static void main(String[] args) {
+		
+		/*média com números inteiros*/
+		
+		int nota1 = 90;
+		int nota2 = 70;
+		int nota3 = 50;
+		int nota4 = 88;
+		
+		System.out.println("Média das notas = " + (nota1 + nota2 + nota3 + nota4) / 4);
+
+	}
+
+}

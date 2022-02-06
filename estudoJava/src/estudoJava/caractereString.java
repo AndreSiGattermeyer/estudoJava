946cd9420cba6bd11f1b4c33e0255af29a559dd3
AndreSiGattermeyer <asigatte@gmail.com>
2022-02-05 21:50:01 -0300

    Tipo String para adicionar texto de qualquer tamanho

---
+++ estudoJava/src/estudoJava/caractereString.java
@@ -0,0 +1,18 @@
+package estudoJava;
+
+public class caractereString {
+
+	public static void main(String[] args) {
+		
+		/*String no Java sempre será um texto de tamanho qualquer*/
+		String textoQualquer = "André Priscila Rafael Samuel Erick Lis pai mãe vó Creusa vó Dina tias primos";
+		
+		String nome;
+		String cpf;
+		String telefone;
+		String endereco;
+		
+		System.out.println(textoQualquer);
+	}
+
+}

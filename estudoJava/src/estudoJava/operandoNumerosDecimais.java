a738db53a018d0a07b50701a1ee2c45cad2b9ce6
AndreSiGattermeyer <asigatte@gmail.com>
2022-02-05 21:29:07 -0300

    Media com números decimais

---
+++ estudoJava/src/estudoJava/operandoNumerosDecimais.java
@@ -0,0 +1,21 @@
+package estudoJava;
+
+public class operandoNumerosDecimais {
+	
+public static void main(String[] args) {
+		
+		/*média com números decimais*/
+		
+		double nota1 = 90;
+		double nota2 = 70;
+		double nota3 = 50;
+		double nota4 = 88;
+		
+		double mediaFinal = (nota1 + nota2 + nota3 + nota4) / 4;
+		
+		System.out.println("Média das notas é " + mediaFinal);
+
+	}
+
+
+}

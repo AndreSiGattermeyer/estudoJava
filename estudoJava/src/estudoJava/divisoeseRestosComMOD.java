e922908739a0851cfa484220dc569e14d55cd3df
AndreSiGattermeyer <asigatte@gmail.com>
2022-02-08 18:23:46 -0300

    resto de divisão

---
+++ estudoJava/src/estudoJava/divisoeseRestosComMOD.java
@@ -0,0 +1,16 @@
+package estudoJava;
+
+public class divisoeseRestosComMOD {
+
+	public static void main(String[] args) {
+		
+		/*Modulo Resto da divisão % */
+		double carros = 9;
+		double pessoas = 2;
+		
+		double resto = carros % pessoas;
+		
+		System.out.println("sobraram exatamente: "+ resto);
+	}
+
+}

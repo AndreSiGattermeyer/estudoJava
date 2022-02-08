80b910eb01ba67eb28ca33c55fc5848e3a67794e
AndreSiGattermeyer <asigatte@gmail.com>
2022-02-08 15:34:24 -0300

    Estrutura de repetição FOR

---
+++ estudoJava/src/estudoJava/estruturaDeRepeticaoFor.java
@@ -0,0 +1,15 @@
+package estudoJava;
+
+public class estruturaDeRepeticaoFor {
+
+	public static void main(String[] args) {
+		/*estrutura de repetição FOR*/ 
+		
+		for (int numero = 10; numero >= 0; numero --) {
+			System.out.println("Número atual é : " + numero);
+			
+		}
+
+	}
+
+}

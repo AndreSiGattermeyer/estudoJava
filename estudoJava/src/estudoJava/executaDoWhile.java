29971f47cdee3986e7324159c2732e52d21619df
AndreSiGattermeyer <asigatte@gmail.com>
2022-02-08 14:35:11 -0300

    Primeiro executa e depois verifica

---
+++ estudoJava/src/estudoJava/executaDoWhile.java
@@ -0,0 +1,16 @@
+package estudoJava;
+
+public class executaDoWhile {
+
+	public static void main(String[] args) {
+		
+		int numero = 0;
+		do { /*primeiro executa e depois verifica*/
+			System.out.println("O número atual é : " + numero);
+			numero++;
+			
+		}while(numero <= 60);
+
+	}
+
+}

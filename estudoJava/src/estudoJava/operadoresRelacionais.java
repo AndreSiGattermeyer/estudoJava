c3fe67461d6e64148674f8eb747ca9c4f1659062
AndreSiGattermeyer <asigatte@gmail.com>
2022-02-08 13:28:02 -0300

    Operadores relacionais
    ==
    !=
    <
    >
    <=
    >=

---
+++ estudoJava/src/estudoJava/operadoresRelacionais.java
@@ -0,0 +1,18 @@
+package estudoJava;
+
+public class operadoresRelacionais {
+
+	public static void main(String[] args) {
+		
+		int nota1 = 91;
+		int nota2 = 91;
+		
+		if (nota1 < nota2) {
+			System.out.println("nota 1 é menor");
+		}else {
+			System.out.println("as notas são iguais");
+		}
+
+	}
+
+}

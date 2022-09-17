3c94daeb56c9e891c75bd1657411212fcec16db8
andre <andre@host.docker.internal>
2022-09-17 13:43:58 -0300

    Formatando datas com o SimpleDateFormat

--- estudoJava/src/modulo_datas/DatasEmJava.java
+++ estudoJava/src/modulo_datas/DatasEmJava.java
@@ -1,10 +1,12 @@
 package modulo_datas;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class DatasEmJava {
 
-		public static void main(String[] args) {
+		public static void main(String[] args) throws ParseException {
 		
 		Date date = new Date();
 		
@@ -23,6 +25,22 @@ public class DatasEmJava {
 		System.out.println("Ano: "  + (date.getYear() + 1900));
 		
 		
+		/*-----------------------Simple Date Format------------------------*/
+		
+		
+		SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd/MM/yyyy HH:mm.ss");
+		
+		System.out.println("Data atual em formato padrão e String: " + simpleDateFormat.format(date));
+		
+		simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm.ss");
+		
+		System.out.println("Data em formato parea banco de dados: " + simpleDateFormat.format(date));
+		
+		simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
+		System.out.println("Objeto Date " + simpleDateFormat.parse("1987-10-18 "));
+		
+		simpleDateFormat = new SimpleDateFormat("dd/MM/yyyy");
+		System.out.println("Objeto Date " + simpleDateFormat.parse("12/06/1984 "));
 	}
 	
 }
